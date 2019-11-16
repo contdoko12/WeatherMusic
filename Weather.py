c1c1a48b7418bfd6129f344ef207ea10347bf37d
contdoko12 <57838558+contdoko12@users.noreply.github.com>
2019-11-16 14:18:31 -0500

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,15 +1,16 @@
 import pyowm
-
+P=input('input a city')
 owm = pyowm.OWM('7f79fb28dc1e878a119f011470868208')
+if P == 'New York' or 'Tampa':
+    location = owm.weather_at_place(P)
+    weather = location.get_weather()
 
-location = owm.weather_at_place('New York')
-weather = location.get_weather()
 
 print(weather)
 
 temp = weather.get_temperature('fahrenheit')
+ 
 
 print(temp)
-
 
 
